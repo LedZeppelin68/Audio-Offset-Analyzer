0c6f8a8cd4bd5028bdb992f79de427f9be17f8d1
Андрей Широчкин <LedZeppelin68@gmail.com>
2023-03-18 23:41:43 +0300

    Добавьте файлы проекта.

---
+++ Audio Offset Analyzer/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Audio_Offset_Analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.chart1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.numericUpDown4);
+            this.panel1.Controls.Add(this.numericUpDown3);
+            this.panel1.Controls.Add(this.numericUpDown2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(794, 84);
+            this.panel1.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea1.CursorX.Interval = 588D;
+            chartArea1.CursorY.Interval = 256D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 93);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "AudioSource1";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "AudioSource2";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series2.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(794, 354);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.textBox1_DragOver);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(3, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox2_DragDrop);
+            this.textBox2.DragOver += new System.Windows.Forms.DragEventHandler(this.textBox2_DragOver);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "source 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(342, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "source 2";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(418, 4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(64, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(519, 3);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            2352,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2352,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDown2.TabIndex = 5;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(519, 25);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            2352,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            2352,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDown3.TabIndex = 5;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(418, 25);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            44100,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            44100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(64, 20);
+            this.numericUpDown4.TabIndex = 6;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(236, 55);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(342, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "offset";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "patch";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Audio Offset Analyzer/Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Audio_Offset_Analyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            chart1.ChartAreas[0].AxisY.Minimum = Int16.MinValue;
+            chart1.ChartAreas[0].AxisY.Maximum = Int16.MaxValue;
+
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 2352;
+
+            int offset = (int)numericUpDown1.Value * 176400 + (int)numericUpDown4.Value * 2352;
+
+            textBox3.Text = offset.ToString();
+        }
+
+        private void textBox1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string source1 = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+
+            numericUpDown1.Value = 0;
+            numericUpDown4.Value = 0;
+
+            int second = (int)numericUpDown1.Value * 75 * 2352;
+
+            textBox1.Text = source1;
+
+            AudioSource1.Clear();
+            DecodeAudio(source1, AudioSource1);
+
+            
+            numericUpDown1.Maximum = AudioSource1.Count / (75 * 2352);
+
+            chart1.Series[0].Points.Clear();
+
+            for (int i = 0; i < 2352; i++)
+            {
+                Int16 sample = (Int16)AudioSource1[second + i * 4];
+                sample += (Int16)(AudioSource1[second + i * 4 + 1] << 8);
+
+                chart1.Series[0].Points.Add(sample);
+            }
+        }
+
+        List<byte> AudioSource1 = new List<byte>();
+        List<byte> AudioSource2 = new List<byte>();
+
+        private void DecodeAudio(string source, List<byte> AudioSource)
+        {
+            using (Process ffmpeg = new Process())
+            {
+                ffmpeg.StartInfo.FileName = "ffmpeg.exe";
+                ffmpeg.StartInfo.Arguments = string.Format("-i \"{0}\" -bitexact -map_metadata -1 -f s16le pipe:1", source);
+                ffmpeg.StartInfo.CreateNoWindow = true;
+                ffmpeg.StartInfo.UseShellExecute = false;
+                ffmpeg.StartInfo.RedirectStandardOutput = true;
+
+                ffmpeg.Start();
+
+                //using (BinaryWriter bw = new BinaryWriter(new FileStream(audio_file + ".wav", FileMode.Create)))
+                //{
+                using (BinaryReader bsr = new BinaryReader(ffmpeg.StandardOutput.BaseStream))
+                {
+                    //bsr.ReadBytes(44);
+
+                    byte[] _temp;
+
+                    do
+                    {
+                        _temp = bsr.ReadBytes(2352);
+                        AudioSource.AddRange(_temp);
+                    }
+                    while (_temp.Length == 2352);
+                }
+                //}
+                ffmpeg.WaitForExit();
+            }
+        }
+
+        private void textBox2_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void textBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            string source2 = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+
+            textBox2.Text = source2;
+
+            int second = (int)numericUpDown1.Value * 75 * 2352;
+
+            AudioSource2.Clear();
+            DecodeAudio(source2, AudioSource2);
+
+            chart1.Series[1].Points.Clear();
+
+            for (int i = 0; i < 2352; i++)
+            {
+                Int16 sample = (Int16)AudioSource2[second + i * 4];
+                sample += (Int16)(AudioSource2[second + i * 4 + 1] << 8);
+
+                chart1.Series[1].Points.Add(sample);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int second = (int)numericUpDown1.Value * 75 * 2352;
+            int offset = (int)numericUpDown4.Value * 2352;
+            int offset1 = (int)numericUpDown2.Value * 4;
+            int offset2 = (int)numericUpDown3.Value * 4;
+
+            textBox3.Text = ((int)numericUpDown1.Value * 352800 + (int)numericUpDown4.Value * 2352).ToString();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            for (int i = 0; i < 2352; i++)
+            {
+                Int16 sample = (Int16)AudioSource1[offset1 + second + offset + i * 4];
+                sample += (Int16)(AudioSource1[offset1 + second + offset + i * 4 + 1] << 8);
+
+                chart1.Series[0].Points.Add(sample);
+
+                sample = (Int16)AudioSource2[offset2 + second + offset + i * 4];
+                sample += (Int16)(AudioSource2[offset2 + second + offset + i * 4 + 1] << 8);
+
+                chart1.Series[1].Points.Add(sample);
+            }
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            int second = (int)numericUpDown1.Value * 75 * 2352;
+            int offset = (int)numericUpDown4.Value * 2352;
+            int offset2 = (int)numericUpDown3.Value * 4;
+
+            //chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            for (int i = 0; i < 2352; i++)
+            {
+                //Int16 sample = (Int16)AudioSource1[second + i * 2];
+                //sample += (Int16)(AudioSource1[second + i * 2 + 1] << 8);
+                //
+                //chart1.Series[0].Points.Add(sample);
+
+                Int16 sample = (Int16)AudioSource2[offset2 + second+ offset + i * 4];
+                sample += (Int16)(AudioSource2[offset2 + second+ offset + i * 4 + 1] << 8);
+
+                chart1.Series[1].Points.Add(sample);
+            }
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            int second = (int)numericUpDown1.Value * 75 * 2352;
+            int offset = (int)numericUpDown4.Value * 2352;
+            int offset1 = (int)numericUpDown2.Value * 4;
+
+            chart1.Series[0].Points.Clear();
+            //chart1.Series[1].Points.Clear();
+
+            for (int i = 0; i < 2352; i++)
+            {
+                Int16 sample = (Int16)AudioSource1[offset1 + second + offset + i * 4];
+                sample += (Int16)(AudioSource1[offset1 + second + offset + i * 4 + 1] << 8);
+
+                chart1.Series[0].Points.Add(sample);
+
+                //Int16 sample = (Int16)AudioSource2[offset + second + i * 2];
+                //sample += (Int16)(AudioSource2[offset + second + i * 2 + 1] << 8);
+                //
+                //chart1.Series[1].Points.Add(sample);
+            }
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            int second = (int)numericUpDown1.Value * 75 * 2352;
+            int offset = ((int)numericUpDown4.Value * 2352);
+            int offset1 = (int)numericUpDown2.Value * 4;
+            int offset2 = (int)numericUpDown3.Value * 4;
+
+            textBox3.Text = ((int)numericUpDown1.Value * 176400 + (int)numericUpDown4.Value * 2352).ToString();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+
+            for (int i = 0; i < 2352; i++)
+            {
+                Int16 sample = (Int16)AudioSource1[offset1 + second + offset + i * 4];
+                sample += (Int16)(AudioSource1[offset1 + second + offset + i * 4 + 1] << 8);
+
+                chart1.Series[0].Points.Add(sample);
+
+                sample = (Int16)AudioSource2[offset2 + second + offset + i * 4];
+                sample += (Int16)(AudioSource2[offset2 + second + offset + i * 4 + 1] << 8);
+
+                chart1.Series[1].Points.Add(sample);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int samples = (int)numericUpDown2.Value;
+            int offset = int.Parse(textBox3.Text);
+            int bytes = Math.Abs(samples) * 4;
+
+
+
+            if(samples < 0)
+            {
+                AudioSource1.RemoveRange(AudioSource1.Count - bytes, bytes);
+                AudioSource1.InsertRange(offset, new byte[bytes]);
+            }
+            else
+            {
+                AudioSource1.RemoveRange(offset, bytes);
+                AudioSource1.AddRange(new byte[bytes]);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string file = textBox1.Text + ".fixed.wav";
+
+            byte[] wavheader = { 0x52, 0x49, 0x46, 0x46, 0xff, 0xff, 0xff, 0xff, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20, 0x10, 0x00, 0x00, 0x00, 0x01, 0x00, 0x02, 0x00, 0x44, 0xAC, 0x00, 0x00, 0x10, 0xB1, 0x02, 0x00, 0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74, 0x61, 0xff, 0xff, 0xff, 0xff };
+
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(file, FileMode.Create)))
+            {
+                bw.Write(wavheader);
+                bw.Write(AudioSource1.ToArray());
+            }
+            
+        }
+    }
+}
